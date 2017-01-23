0a1fe9787cc6b42631883a91af4643a9c0a2cdc2
Michał Łuszczyk <m0@qed.ai>
2017-01-23 18:34:31 +0100

    Django 1.9 compatibility.
    Change-Id: I333a413dbef89a789709219e28ea95ffec484755

---
+++ README.md
@@ -0,0 +1,5 @@
+# django_cas_binder
+
+## Compatibility
+
+Works with Python 2, Python 3 and Django not older than 1.9.
--- setup.py
+++ setup.py
@@ -4,13 +4,29 @@ from distutils.core import setup
 
 setup(
     name='django_cas_binder',
-    version='1.5',
+    version='1.5.1',
     description=(
         'A thin wrapper around django-cas-ng that allows to use identifier '
         'other than username.'
     ),
-    author='',
+    author='Quantitative Engineering Design Inc.',
     author_email='',
     url='',
     packages=['django_cas_binder', 'django_cas_binder.migrations'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Framework :: Django',
+    ],
+    install_requires=[
+        "django>=1.9.0"
+    ]
 )
